5c37454e187a0cfffb66c1cfa1291ffcd2dad277
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2025-02-16 22:10:36 +0530

    .

---
+++ Graphs/kruskal's.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdio.h>
+
+struct node{
+    int vertex;
+    int weight;
+    struct node *next;
+};
+
+struct Graph{
+    int vertices;
+    struct node **adjacency;
+};
+
