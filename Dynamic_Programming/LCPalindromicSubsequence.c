78b3b75c847aac363849fee33f1e4475141bd546
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2025-10-08 19:29:58 +0530

    Length_of_Longest_Common_Palindromic_Subsequence

---
+++ Dynamic_Programming/LCPalindromicSubsequence.c
@@ -0,0 +1,33 @@
+// Code to find out the length of Longest Common Palindromic Subsequence using recursion
+#include<stdio.h>
+
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
+int LCPS(int n, int m, char str[]){
+    if(n > m){
+        return 0;
+    }
+    if( n == m ){
+        return 1;
+    }
+    if(str[n] == str[m]){
+        return LCPS(n + 1 , m - 1 , str) + 2;
+    }
+    return max(LCPS(n + 1 , m , str), LCPS(n , m - 1, str));
+}
+
+int main(){
+    int n;
+    printf("Enter length of string : ");
+    scanf("%d",&n);
+    char str[n];
+    getchar();
+    printf("Enter string : ");
+    for(int i = 0 ; i < n ; i++){
+        scanf("%c",&str[i]);
+    }
+    printf("Longest Palindromic Subsequence length is : %d\n",LCPS(0,n-1,str));
+    return 0;
+}
